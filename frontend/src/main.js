e989e5aad69a1c0ed30f2a5c17110f9f890b9217
Furkan Sade Uckun <88970339+furkansade@users.noreply.github.com>
2024-02-17 17:36:51 +0300

    import-bootstrap

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,16 +1,7 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+
 import { createApp } from "vue";
 import App from "@/App.vue"
 
 const app = createApp(App);
 app.mount('#app');
-
-// const app = createApp({
-//     data() {
-//         return {
-//             name: "sadeSoftware",
-//         };
-//     },
-//     template: `<h2>Our company name is {{name}}!</h2>`,
-// });
-
-// app.mount('#app');
